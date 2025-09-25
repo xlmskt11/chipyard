5dc20df99b3e56a0fd6553bb1664ebbaf6e430c4
xlmskt11 <dwc06209@naver.com>
2025-09-25 22:38:21 +0900

    chipyard: code changes + bump gemmini to ExtLoopnRS

--- generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
+++ generators/chipyard/src/main/scala/config/RoCCAcceleratorConfigs.scala
@@ -12,9 +12,10 @@ class GemminiRocketConfig extends Config(
   // new gemmini.DefaultGemminiConfig(0, 16, 16, 256, 64) ++                            // use Gemmini systolic array GEMM accelerator
   // new gemmini.DefaultGemminiConfig(1, 16, 16, 256, 64) ++                            // use Gemmini systolic array GEMM accelerator
   // new gemmini.DefaultGemminiConfig(2, 16, 16, 256, 64) ++                            // use Gemmini systolic array GEMM accelerator
-  // new gemmini.DefaultGemminiConfig(3, 32, 32, 256, 64) ++                            // use Gemmini systolic array GEMM accelerator
-  new gemmini.MultiDefaultGemminiConfig(4, 16, 16) ++
-  new freechips.rocketchip.subsystem.WithNBanks(4) ++
+  // new gemmini.DefaultGemminiConfig(3, 16, 16, 256, 64) ++                            // use Gemmini systolic array GEMM accelerator
+  // new freechips.rocketchip.subsystem.WithoutTLMonitors() ++
+  new gemmini.MultiDefaultGemminiConfig(16, 16, 64, 128, 64, 32) ++
+  new freechips.rocketchip.subsystem.WithNBanks(2) ++
   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
   new chipyard.config.WithSystemBusWidth(128) ++
   new chipyard.config.AbstractConfig)
